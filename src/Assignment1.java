9d10c91d2e621594874d8ad1dd81fb28c73a9a90
ivldheeraj <ivldheeraj@gmail.com>
2021-01-05 13:07:55 +0530

    assignments

---
+++ src/Assignment1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		String str1 = "Hello Dear Friend";
+		//Output as  :  Friends Dear Hello
+		String output = reverseString(str1);
+		System.out.println(output);
+		
+
+	}
+
+	private static String reverseString(String input) {
+		
+		String[] word = input.split(" ");
+
+		String reversedString ="";
+		
+		for(int i=0;i<word.length;i++)
+		{
+			if(i == word.length-1)
+				reversedString = word[i]+ reversedString;
+			else
+				reversedString = " " + word[i]+reversedString;
+			   	
+			
+		}
+		
+		return reversedString;
+	}
+
+}
